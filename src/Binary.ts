abd14e2515088e88b85ae4fc57ae530b293c565e
Jeeyong Um <conr2d@proton.me>
2023-05-17 16:43:39 +0900

    chore: Extract @noir/types from @noir/api

---
+++ src/Binary.ts
@@ -0,0 +1,35 @@
+// Copyright (C) 2023 Haderech Pte. Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+import type { AnyString, AnyU8a, Registry } from '@polkadot/types-codec/types';
+
+import { base64urlToU8a, compactAddLength, hexToU8a, isHex, isString, isU8a, u8aToBase64url, u8aToU8a } from '@noir/util';
+import { Bytes } from '@polkadot/types';
+
+export function decodeBinary (value?: AnyU8a | AnyString): Uint8Array {
+  if (isU8a(value) || Array.isArray(value)) {
+    return u8aToU8a(value);
+  } else if (!value) {
+    return new Uint8Array();
+  } else if (isHex(value)) {
+    return hexToU8a(value);
+  } else if (isString(value)) {
+    return compactAddLength(base64urlToU8a(value.toString()));
+  }
+
+  throw new Error(`Unknown type passed to binary decoder, found typeof ${typeof value}`);
+}
+
+export class Binary extends Bytes {
+  constructor (registry: Registry, value?: AnyU8a) {
+    super(registry, decodeBinary(value));
+  }
+
+  public override toHuman (): string {
+    return u8aToBase64url(this.toU8a(true));
+  }
+
+  public override toRawType (): string {
+    return 'Binary';
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// Copyright (C) 2023 Haderech Pte. Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+export * from './AccountName.js';
+export * from './Binary.js';
+export { MultiAddress } from './MultiAddress.js';
+export * from './UniversalAddress.js';
+
+export * from '@polkadot/types';
