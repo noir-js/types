edf34fecdf0ad40a210d8e2b52700e3addeb5df7
Jeeyong Um <conr2d@proton.me>
2023-07-07 01:24:37 +0900

    test: Add test for MultiAddress construction

---
+++ src/MultiAddress.spec.ts
@@ -0,0 +1,36 @@
+// Copyright (C) 2023 Haderech Pte. Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+import { describe, expect, it } from 'vitest';
+
+import { hexToU8a, u8aConcat } from '@pinot/util';
+import { TypeRegistry } from '@polkadot/types';
+
+import { MultiAddress } from './MultiAddress.js';
+import { UniversalAddress } from './UniversalAddress.js';
+
+describe('MultiAddress', (): void => {
+  const registry = new TypeRegistry();
+
+  registry.register({
+    MultiAddress,
+    'AccountId': UniversalAddress
+  });
+
+  const k1 = 'u5wECOvHh76TR4a1cueOWfpjpAdr803xEzwv7bCFpl_XuUd8';
+  const b2 = 'uoOQCIO_a-ocLu6rnJvG859XLBNZM4u32LX0JVG94-hpGvzOi';
+
+  describe('decoding', (): void => {
+    it('can decode a universal address', (): void => {
+      const m1 = registry.createType<MultiAddress>('MultiAddress', k1);
+      expect(m1.type).toEqual('Id');
+      expect(m1.value.toHuman()).toEqual(k1);
+      expect((m1.value as UniversalAddress).kind).toEqual('secp256k1');
+
+      const m2 = registry.createType<MultiAddress>('MultiAddress', b2);
+      expect(m2.type).toEqual('Id');
+      expect(m2.value.toHuman()).toEqual(b2);
+      expect((m2.value as UniversalAddress).kind).toEqual('blake2b_256');
+    });
+  });
+});
