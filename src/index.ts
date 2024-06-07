2d611aa93d8ca29b35636f66134dbd268701ef06
Jeeyong Um <conr2d@proton.me>
2024-06-07 19:08:26 +0800

    chore: Bump version to 0.3.0 (#11)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 // Copyright (C) 2023 Haderech Pte. Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
-export * from './AccountName.js';
 export * from './Binary.js';
-export { MultiAddress } from './MultiAddress.js';
-export * from './UniversalAddress.js';
 
 export * from '@polkadot/types';
